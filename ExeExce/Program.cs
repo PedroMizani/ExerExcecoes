24b84bf5892677eddda543ac355d1785c20d8ee9
PedroMizani <pedromizani@outlook.com>
2023-04-09 13:56:02 -0300

    Exercicio de Exceções do curso C#

---
+++ ExeExce/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExeExce.Entities;
+using ExeExce.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace ExeExce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter account data: ");
+            Console.WriteLine();
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            try
+            {
+                account.Withdraw(withdraw);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
